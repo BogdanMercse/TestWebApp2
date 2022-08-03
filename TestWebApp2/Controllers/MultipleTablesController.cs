7b6ddd16a425be20498fe4655ba19f6b06acb74f
Bogdan Mercse <m3rtz3@yahoo.com>
2022-08-03 09:53:28 +0300

    first commit

---
+++ TestWebApp2/Controllers/MultipleTablesController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestWebApp2.Controllers
+{
+   // public class CustomerController: Controller
+    public class MultipleTablesController : Controller
+    {
+        // GET: MultipleTables
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        MosquitoNetEntities mosquitoNetEntities = new MosquitoNetEntities();
+        public JsonResult GetCustomerNames()
+        {
+            List<Customer> customerList = mosquitoNetEntities.Customers.ToList();
+
+            return Json(customerList,JsonRequestBehavior.AllowGet );
+        }
+
+        public JsonResult GetMosquitoNetRequests()
+        {
+            List<MosquitoNetRequest> mosquitoNetRequestsList = mosquitoNetEntities.MosquitoNetRequests.ToList();
+
+            return Json(mosquitoNetRequestsList, JsonRequestBehavior.AllowGet);
+        }
+        //[Route("Delete")]
+        [Route("MultipleTables/Delete")]
+        public string DeleteCustomer(Customer customer)
+        {
+            try
+            {
+                Customer customerDb= mosquitoNetEntities.Customers.SingleOrDefault(e => e.customerId == customer.customerId);
+                mosquitoNetEntities.Configuration.ValidateOnSaveEnabled = false;
+                mosquitoNetEntities.Customers.Attach(customerDb);
+                mosquitoNetEntities.Entry(customerDb).State = EntityState.Deleted;
+                mosquitoNetEntities.SaveChanges();
+            }
+            catch(Exception ex)
+            {
+                return "Customer could not be deleted. Error:" + ex.Message;
+            }
+            finally
+            {
+                mosquitoNetEntities.Configuration.ValidateOnSaveEnabled = true;
+            }
+           
+            return "Customer deleted";
+        }
+
+        public string CreateCustomer(Customer customer)
+        {
+            try
+            {
+                mosquitoNetEntities.Customers.Add(customer);
+                mosquitoNetEntities.SaveChanges();
+                return "Customer created";
+            }
+            catch (Exception ex)
+            {
+                return "Customer could not be created. Error:" + ex.Message;
+            }
+        }
+
+        public JsonResult CreateCustomerJson(Customer customer)
+        {
+            try
+            {
+                mosquitoNetEntities.Customers.Add(customer);
+                mosquitoNetEntities.SaveChanges();
+                return Json(customer,JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception( "Customer could not be created. Error:" + ex.Message);
+            }
+        }
+
+        public string CreateNetRequest(MosquitoNetRequest request)
+        {
+            try
+            {
+                mosquitoNetEntities.MosquitoNetRequests.Add(request);
+                mosquitoNetEntities.SaveChanges();
+                return "Request created";
+            }
+            catch (Exception ex)
+            {
+                return "Request could not be created. Error:" + ex.Message;
+            }
+        }
+
+        public string CreateCustomerAndNetRequest(Customer customer, MosquitoNetRequest request)
+        {
+            try
+            {
+                //orderRepository.AddOrder(Customer)
+                mosquitoNetEntities.Customers.Add(customer);
+                mosquitoNetEntities.SaveChanges();
+
+                request.customerId = customer.customerId;
+                mosquitoNetEntities.MosquitoNetRequests.Add(request);
+                mosquitoNetEntities.SaveChanges();
+                return "Customer and Request created";
+            }
+            catch (Exception ex)
+            {
+                return "Customer and Request could not be created. Error:" + ex.Message;
+            }
+        }
+
+        public string UpdateCustomer(Customer customer)
+        {
+            try
+            {
+                mosquitoNetEntities.Configuration.ValidateOnSaveEnabled = false;
+                mosquitoNetEntities.Customers.Attach(customer);
+                mosquitoNetEntities.Entry(customer).State = EntityState.Modified;
+                mosquitoNetEntities.Configuration.ValidateOnSaveEnabled = true;
+                mosquitoNetEntities.SaveChanges();
+                return "Customer updated";
+            }
+            catch (Exception ex)
+            {
+                return "Customer could not be updated. Error:" + ex.Message;
+            }
+        }
+    }
+}
---
+++ TestWebApp2/Scripts/angularScripts.js
@@ -0,0 +1,105 @@
+﻿var app = angular.module("MyApp", []);
+app.controller("MyController", function ($scope, $http) {
+
+    $scope.updateFormVisible = false;
+
+    $scope.GetCustomerNames = function () {
+        $http(
+            {
+                method: 'GET',
+                url: '/MultipleTables/GetCustomerNames'
+            }).then(function (customerTable) {
+                $scope.customerList = customerTable.data;
+            });
+    }
+
+    $http(
+        {
+            method: 'GET',
+            url: '/MultipleTables/GetMosquitoNetRequests'
+        }).then(function (mosquitoTable) {
+            $scope.mosquitoNetRequestsList = mosquitoTable.data;
+        });
+
+    $scope.DeleteCustomer = function (customerId) {
+        $scope.Customer = {};
+        $scope.Customer.customerId = customerId;
+        $http({
+            method: "post",
+            url: "/MultipleTables/Delete",
+            datatype: "json",
+            data: JSON.stringify($scope.Customer)
+        }).then(function (response) {
+            alert(response.data);
+            $scope.GetCustomerNames();
+        })
+    };
+
+    $scope.CreateCustomer = function (Customer) {
+        $scope.Customer = {};
+        $scope.Customer.customerName = $scope.customerName;
+        $scope.Customer.customerEmail = $scope.customerEmail;
+        $scope.Customer.customerPhone = $scope.customerPhone;
+
+        $http({
+            method: "post",
+            url: "/MultipleTables/CreateCustomer",
+            datatype: "json",
+            data: JSON.stringify($scope.Customer)
+        }).then(function (response) {
+            alert("response:" + response.data);
+            $scope.Customer.customerName = "";
+            $scope.Customer.customerEmail = "";
+            $scope.Customer.customerPhone = "";
+            $scope.GetCustomerNames();
+        })
+    };
+
+    $scope.CreateNetRequest = function (Customer) {
+        $scope.MosquitoNetRequest = {};
+        $scope.MosquitoNetRequest.customerId = Customer.customerId;
+        $scope.MosquitoNetRequest.length = $scope.length;
+        $scope.MosquitoNetRequest.height = $scope.height;
+
+        $http({
+            method: "post",
+            url: "/MultipleTables/CreateNetRequest",
+            datatype: "json",
+            data: JSON.stringify($scope.MosquitoNetRequest)
+        }).then(function (response) {
+            alert(response.data);
+            $scope.MosquitoNetRequest.customerId = "";
+            $scope.MosquitoNetRequest.length = "";
+            $scope.MosquitoNetRequest.height = "";
+        })
+    };
+
+    $scope.ShowUpdateForm = function (Customer) {
+        $scope.updateFormVisible = true;
+        $scope.customerIdUpdate = Customer.customerId;
+        $scope.customerNameUpdate = Customer.customerName;
+        $scope.customerEmailUpdate = Customer.customerEmail;
+        $scope.customerPhoneUpdate = Customer.customerPhone;
+    };
+
+    $scope.UpdateCustomer = function (Customer) {
+        $scope.Customer = {};
+        $scope.Customer.customerId = $scope.customerIdUpdate;
+        $scope.Customer.customerName = $scope.customerNameUpdate;
+        $scope.Customer.customerEmail = $scope.customerEmailUpdate;
+        $scope.Customer.customerPhone = $scope.customerPhoneUpdate;
+
+        $http({
+            method: "post",
+            url: "/MultipleTables/UpdateCustomer",
+            datatype: "json",
+            data: JSON.stringify($scope.Customer)
+        }).then(function (response) {
+            alert(response.data);
+            $scope.Customer.customerName = "";
+            $scope.Customer.customerEmail = "";
+            $scope.Customer.customerPhone = "";
+            $scope.GetCustomerNames();
+        })
+    };
+});
